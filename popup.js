2eeaa895f416720687b6289b634aab8fb3b95212
Jasmine Elkins <jasmine.elkins@gmail.com>
2022-05-17 11:51:10 -0400

    POST creates new Article in db with current tab url

--- popup.js
+++ popup.js
@@ -6,17 +6,30 @@ addButton.addEventListener("click", function () {
 
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     //get URL of current tab
-    let url = tabs[0].url;
+    let current_url = tabs[0].url;
 
-    console.log(url);
+    console.log(current_url);
 
     // send url to save in back end: fetch POST
+    fetch(`http://localhost:3000/articles`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        url: current_url,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error.message));
   });
 
-  fetch(`http://localhost:3000/articles`)
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error.message));
+  //   fetch(`http://localhost:3000/articles`)
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data))
+  //     .catch((error) => console.log(error.message));
 });
 
 // chrome.tabs.onActivated.addListener(function (activeInfo) {
