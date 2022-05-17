5fc0243a5f22b34efdba5742a59090e936006b93
Jasmine Elkins <jasmine.elkins@gmail.com>
2022-05-17 14:11:50 -0400

    GET user and add Article to that user.id (using default newsletter 1, seed)

--- popup.js
+++ popup.js
@@ -10,26 +10,31 @@ addButton.addEventListener("click", function () {
 
     console.log(current_url);
 
-    // send url to save in back end: fetch POST
-    fetch(`http://localhost:3000/articles`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({
-        url: current_url,
-      }),
-    })
+    // GET current User:
+    fetch(`http://localhost:3000/me`)
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((userObj) => {
+        console.log(userObj);
+
+        // POST create new Article with url and user_id
+        fetch(`http://localhost:3000/articles`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify({
+            url: current_url,
+            user_id: userObj.id,
+            newsletter_id: 1,
+          }),
+        })
+          .then((res) => res.json())
+          .then((data) => console.log(data))
+          .catch((error) => console.log(error.message));
+      })
       .catch((error) => console.log(error.message));
   });
-
-  //   fetch(`http://localhost:3000/articles`)
-  //     .then((res) => res.json())
-  //     .then((data) => console.log(data))
-  //     .catch((error) => console.log(error.message));
 });
 
 // chrome.tabs.onActivated.addListener(function (activeInfo) {
